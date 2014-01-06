bc908cab88f9d7e40840100cd5f90d009cebd1c5
Frank Li <lxf20035@msn.com>
2014-01-06 02:08:28 -0500

    Added Google Maps iOS SDK Added NSString Categories Added Restaurant Map View Controller

---
+++ UW Food Services/RestaurantMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantMapViewController.h
+//  UW Food Services
+//
+//  Created by Frank Li on 1/5/2014.
+//  Copyright (c) 2014 Xuefei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantMapViewController : UIViewController
+
+@end
---
+++ UW Food Services/RestaurantMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  RestaurantMapViewController.m
+//  UW Food Services
+//
+//  Created by Frank Li on 1/5/2014.
+//  Copyright (c) 2014 Xuefei Li. All rights reserved.
+//
+
+#import "RestaurantMapViewController.h"
+
+@interface RestaurantMapViewController ()
+
+@end
+
+@implementation RestaurantMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
