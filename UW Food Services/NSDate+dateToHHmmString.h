f60a56d3c2766f3af132ec4cc81505ab7a5b301b
Frank Li <lxf20035@msn.com>
2014-01-07 03:42:59 -0500

    Added assets. Added todo.txt. Added categories.

---
+++ UW Food Services/NSDate+dateToHHmmString.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+dateToHHmmString.h
+//  UW Food Services
+//
+//  Created by Frank Li on 1/6/2014.
+//  Copyright (c) 2014 Xuefei Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (dateToHHmmString)
+
+@end
---
+++ UW Food Services/NSDate+dateToHHmmString.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+dateToHHmmString.m
+//  UW Food Services
+//
+//  Created by Frank Li on 1/6/2014.
+//  Copyright (c) 2014 Xuefei Li. All rights reserved.
+//
+
+#import "NSDate+dateToHHmmString.h"
+
+@implementation NSDate (dateToHHmmString)
+
+@end
